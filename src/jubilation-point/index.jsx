fa62cdfb52dc05a4881a7466827226e5f118bc39
Corey Flynn <flynn.corey@gmail.com>
2016-10-10 09:36:34 -0400

    Moves JSX files to .jsx extension

---
+++ src/jubilation-point/index.jsx
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+import { default as theme } from '../jubilation-theme';
+
+type Props = { x: number, y: number, size?: number, color?: string };
+
+/**
+ * A single data point. Returns an SVG circle element
+ * @param {Props} { x, y, size = 10, color = theme.colors[0] }
+ * @returns {}
+ */
+export default function JubilationPoint({ x, y, size = 10, color = theme.colors[0] }: Props) {
+  return <circle cx={x} cy={y} r={size} fill={color} />;
+}
