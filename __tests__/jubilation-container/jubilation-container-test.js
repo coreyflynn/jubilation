2c56676c3b2d909862dd386eea9b6bcedf1cde93
Corey Flynn <flynn.corey@gmail.com>
2016-10-10 09:36:05 -0400

    Structure updates

--- __tests__/jubilation-container/jubilation-container-test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import JubilationContainer from '../../src/jubilation-container';
-
-it('should render an SVG tag', () => {
-  expect(shallow(<JubilationContainer />).find('svg').length).toBe(1);
-});
