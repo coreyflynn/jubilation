da14f6240f142db4042da2a592473efd22b2c752
Corey Flynn <flynn.corey@gmail.com>
2016-11-14 08:48:09 -0500

    Fixes some lint errors

--- src/helpers/context.js
+++ src/helpers/context.js
@@ -1,7 +1,7 @@
 // @flow
 import JubilationTheme from '../jubilation-theme';
 
-export default function getContext(context: JubilationContext) : JubilationContext {
+export default function getContext(context: JubilationContext): JubilationContext {
   if (context) return context;
   function scale(x: number): number { return x; }
   scale.domain = function domain(): number[] { return [0, 100]; };
